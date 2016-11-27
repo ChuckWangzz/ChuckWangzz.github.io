f58bc65d0ccae18ed066955b50381bec046ce6bc
EngineerWang <851350628@qq.com>
2016-11-27 19:24:49 +0800

    个人网站前端网页更新

---
+++ application/views/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<meta charset="UTF-8">
+	<title>profile</title>
+	<link rel="stylesheet" href="../../css/font-awesome.min.css">
+	<link rel="stylesheet" href="../../css/common.css">
+	<link rel="stylesheet" href="../../css/index.css">
+</head>
+<body>
+	<div id="contain" class="clearfix">
+		<div id="profile" class="style">
+			<div class="wrapper pad">
+				<div id="profile-head">
+					<img src="../../images/img/down.png" alt="头像">
+				</div>
+				<div id="profile-info">
+					<h1 id="in-name">Wang Cong</h1>
+					<h4 id="in-major">GRAPHIC &amp; WEB DESIGNER</h4>
+					<h4 id="in-address">Haerbin , CHINA</h4>
+				</div>
+				<div id="profile-desc">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac risus nibh. Donec adipiscing luctus tur</p>
+					<p>More Templates  - Collect from</p>
+				</div>
+				<div id="profile-social">
+					<h4>My Social Profiles</h4>
+					<div id="pro-so-icon">
+						<a href="#">
+							<i class="fa fa-facebook"></i>
+						</a>
+						<a href="#">
+							<i class="fa fa-twitter"></i>
+						</a>
+						<a href="#">
+							<i class="fa fa-linkedin"></i>
+						</a>
+						<a href="#">
+							<i class="fa fa-dribbble"></i>
+						</a>
+						<a href="#">
+							<i class="fa fa-foursquare"></i>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="content">
+            <div id="about-me"></div>
+            <div id="personal-info"></div>
+            <div id="my-works"></div>
+            <div id="my-blog"></div>
+            <div id="contact-me"></div>
+        </div>
+		<ul id="nav" class="style">
+			<li class="select">
+				<a href="#" class="fa fa-user nav-icon">
+				</a>
+				<span>PROFILE</span>
+			</li>
+			<li>
+				<a href="#" class="fa fa-tasks nav-icon">
+				</a>
+				<span>RESUME</span>
+			</li>
+			<li>
+				<a href="#" class="fa fa-briefcase nav-icon">
+				</a>
+				<span>PORTFOLIO</span>
+			</li>
+			<li>
+				<a href="#" class="fa fa-bullhorn nav-icon">
+				</a>
+				<span>BLOG</span>
+			</li>
+			<li>
+				<a href="#" class="fa fa-envelope nav-icon">
+				</a>
+				<span>CONTACT</span>
+			</li>
+		</ul>
+		
+	</div>
+</body>
+</html>
